be20a574adb250594bfb4e9fd6b23f73b6c43652
David Godfrey <david.godfrey99@gmail.com>
2025-12-13 00:49:43 -0500

    Update README

---
+++ README.md
@@ -0,0 +1,44 @@
+
+## Market Feed Server
+Market Data Server receives serialized packets over a TCP connection. The packets represent quotes and trades for stocks. As the data is passed through a VWAP calculation is performed, and an order is sent to another socket connection when the order condition is ideal.
+
+## Compiler requirements
+The program works for C++17 compilers or greater.
+
+## Details
+Information about the name of the stock, the maximum order quantity, and the VWAP window are passed as command line arguments. The feed server recieves quotes and trades and keeps track of the timestamps, prices, and quantities, and uses this to determine when a order can be sent to the client. There must be at least one trade processed before an order can be made, because a trade has information that is incorporated into the VWAP calculation which determines when a new order can be made.
+
+On reciept of a new quote or trade, its timestamp is compared against the earliest timestamp seen in the VWAP window. If the difference (in seconds) is greater than the max window time period given as a command line argument, we check if a trade has been seen earlier. If so, a new order can be made.
+
+I implemented a few methods for `std::optional` that were added in C++23, and added a new one of my own. This allows me to use convienent methods such as `and_then` or `or_else` when dealing with optional values.
+
+The format classes are contained in a namespace `Schema`. Padding is disabled for these classes with a `#pragma pack` statement.
+
+RAII is used for handling sockets. 
+
+## Building
+From the root project directory run `make`:
+```bash
+make
+```
+The resulting executables will appear in the project's `bin` directory. 
+
+Then run the program:
+```bash
+make run_main
+```
+ By default it will pass the following command line arguments to `main.cpp`. If you want custom behavior you will have to modify the Makefile or execute `./bin/main` yourself:
+ 
+```bash
+IBM B 100 30 127.0.0.1 5000 127.0.0.1 5001
+```
+
+## Test
+
+The program can be tested by running the following. Sample packets will be sent to main  from the server and orders will be sent to the client:
+```bash
+make
+make run_server # run on seperate command line
+make run_client # run on seperate command line
+make run_main # run on seperate command line
+```
---
+++ program/program.cpp
@@ -0,0 +1,327 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <optional>
+#include <functional>
+#include <type_traits>
+#include <cerrno>
+#include <cassert>
+#include <variant>
+
+namespace detail {
+    template<class T>
+    struct optional : std::optional<T> {
+       using std::optional<T>::optional;
+       using std::optional<T>::operator=;
+
+       template<class Callable>
+       auto and_then(Callable&& callable) {
+        if (*this) {
+            std::invoke(std::forward<Callable>(callable), *this, this->value());
+        }
+	return *this;
+       }
+
+       template<class Callable>
+	auto value_if(Callable&& callable) {
+		if (std::forward<Callable>(callable)(this->value())) {
+			return *this;
+		}
+		return detail::optional<T>{};
+	}
+       template<class Callable>
+       auto or_else(Callable&& callable) {
+        if (!*this) {
+            std::forward<Callable>(callable)(*this);
+        }
+	return *this;
+       }
+    };
+}
+
+struct connection {
+        explicit connection(std::string_view ip, std::string_view port) :
+                sock{socket(AF_INET, SOCK_STREAM, 0)}
+        {
+                auto p = static_cast<unsigned short>(std::stoul(port.data()));
+                sockaddr_in addr{
+                        .sin_family = AF_INET,
+                        .sin_port = htons(p)
+                };
+
+                inet_pton(AF_INET, ip.data(), &addr.sin_addr);
+                int result = connect(sock, (sockaddr*)&addr, sizeof(addr));
+		success = result == 0;
+		assert(success);
+        }
+
+        explicit operator bool() const { return success; }
+
+        int handle() const { return sock; }
+
+        ~connection() {
+                close(sock);
+        }
+private:
+        int sock;
+        bool success;
+};
+
+static constexpr std::size_t symbol_length = 8;
+
+struct Config {
+        std::string_view symbol;
+        char side;
+        std::uint32_t max_order_size;
+        std::uint32_t vwap_window_period;
+        connection market_data_socket;
+        connection order_connection_socket;
+};
+
+namespace Schema {
+	#pragma pack(push, 1)
+	struct Quote {
+        	char symbol[symbol_length];
+        	std::uint64_t timestamp;
+        	std::uint32_t bid_quantity, bid_price;
+        	std::uint32_t ask_quantity, ask_price;
+	};
+
+	struct Trade {
+        	char symbol[symbol_length];
+        	std::uint64_t timestamp;
+        	std::uint32_t quantity;
+        	std::uint32_t price;
+	};
+
+	struct Order {
+        	char symbol[symbol_length];
+        	std::uint64_t timestamp;
+        	char side;
+        	std::uint32_t quantity;
+        	std::uint32_t price;
+	};
+	#pragma pack(pop)
+
+        struct Packet {
+                #pragma pack(push, 1)
+                struct Header {
+                        std::uint8_t length;
+                        std::uint8_t type;
+                } header;
+                #pragma pack(pop)
+                std::variant<Quote, Trade> body;
+        };
+}
+
+detail::optional<Schema::Trade> parse_trade_format(int sockfd, std::uint8_t totalBytes) {
+        Schema::Trade t{};
+        std::size_t bytes = 0;
+        bytes += read(sockfd, t.symbol, symbol_length);
+        bytes += read(sockfd, &t.timestamp, sizeof(t.timestamp));
+	bytes += read(sockfd, &t.quantity, sizeof(t.quantity));
+        bytes += read(sockfd, &t.price, sizeof(t.price));
+
+	std::cout << "Trade Bytes read: " << bytes << '\n';
+        if (bytes == totalBytes)
+            return t;
+        return std::nullopt;
+}
+
+detail::optional<Schema::Quote> parse_quote_format(int sockfd, std::uint8_t totalBytes) {
+        Schema::Quote q{};
+        std::size_t bytes = 0;
+        bytes += read(sockfd, q.symbol, symbol_length);
+	std::cout << "symbol bytes read: " << bytes << ", symbol = " << q.symbol << '\n';
+        bytes += read(sockfd, &q.timestamp, sizeof(q.timestamp));
+        bytes += read(sockfd, &q.bid_quantity, sizeof(q.bid_quantity));
+        bytes += read(sockfd, &q.bid_price, sizeof(q.bid_price));
+        bytes += read(sockfd, &q.ask_quantity, sizeof(q.ask_quantity));
+        bytes += read(sockfd, &q.ask_price, sizeof(q.ask_price));
+        std::cout << "Quote bytes read: " << bytes << '\n';
+	if (bytes == totalBytes)
+            return q;
+        return std::nullopt;
+}
+
+auto parse_quote(int sockfd, std::string_view symbol, std::uint8_t totalBytes) {
+        // only set if the symbol is equal to config.symbol
+        auto quote = parse_quote_format(sockfd, totalBytes);
+	std::cout << "New Quote: \n";
+	std::cout << "symbol: " << quote->symbol << '\n';
+	std::cout << "timestamp: " << quote->timestamp << '\n';
+	std::cout << "bid_quantity: " << quote->bid_quantity << '\n';
+	std::cout << "bid_price: " << quote->bid_price << '\n';
+	std::cout << "ask_quantity: " << quote->ask_quantity << '\n';
+	std::cout << "ask_price: " << quote->ask_price << '\n';
+        return quote.value_if([&](Schema::Quote const& q) {
+                return strncmp(q.symbol, symbol.begin(), symbol_length) == 0;
+        });
+}
+
+auto parse_trade(int sockfd, std::string_view symbol, std::uint8_t totalBytes) {
+        auto trade = parse_trade_format(sockfd, totalBytes);
+	std::cout << "New Trade: \n";
+	std::cout << "symbol: " << trade->symbol << '\n';
+	std::cout << "timestamp: " << trade->timestamp << '\n';
+	std::cout << "quantity: " << trade->quantity << '\n';
+	std::cout << "price: " << trade->price << '\n';
+
+        return trade.value_if([&](Schema::Trade const& t) {
+                return strncmp(t.symbol, symbol.begin(), symbol_length) == 0;
+        });
+}
+
+auto process_order(std::string_view symbol, char side, std::uint64_t timestamp, double vwap, std::optional<Schema::Quote>& quote) {
+	std::uint32_t price, *quantity;
+	bool betterThan = false;
+
+	Schema::Order order{};
+        strncpy(order.symbol, symbol.data(), symbol.size());
+        order.timestamp = timestamp;
+        order.side = side;
+
+	if (side == 'B') {
+		price = quote->ask_price;
+		quantity = &quote->ask_quantity;
+		betterThan = quote->ask_price < vwap;
+	} else {
+		price = quote->bid_price;
+		quantity = &quote->bid_quantity;
+		betterThan = quote->bid_price > vwap;
+	}
+	return [=](auto&& callable) mutable {
+		if (*quantity > 0 && betterThan) {
+			(decltype(callable)(callable))(order, price, *quantity);
+			*quantity -= order.quantity;
+		}
+	};
+}
+
+template<class Packet>
+struct Feed {
+	Feed() = delete;
+	Feed(Config& config) :
+		p_args(config),
+		market_sockfd(p_args.market_data_socket.handle())
+	{ }
+
+	using Header = typename Packet::Header;
+	using Trade = Schema::Trade;
+	using Quote = Schema::Quote;
+	using Order = Schema::Order;
+
+	void parse(Quote& quote, std::string_view symbol, std::size_t len) {
+		auto temp = parse_quote(market_sockfd, symbol, len);
+
+		if (temp) {
+                	std::cout << "Successfully parsed quote\n";
+                        current_quote = temp;
+			quote = temp.value();
+			std::cout << "Hello\n";
+                };
+	}
+
+	void parse(Trade& trade, std::string_view symbol, std::size_t len) {
+		auto temp = parse_trade(market_sockfd, p_args.symbol, len);
+                temp.and_then([&](auto&& self, Trade const& t) {
+                	std::cout << "Updating values\n";
+                        pq_sum += t.price * t.quantity;
+                        q_sum += t.quantity;
+			trade = t;
+			seen_trade = true;
+                }).or_else([&] (auto&& self) {
+                	perror("Something went wrong parsing the trade. Aborting...");
+                        std::exit(1);
+                });
+	}
+
+	void forward() {
+		if (!p_args.market_data_socket)
+			return;
+
+                ssize_t n = read(market_sockfd, &current_packet.header, sizeof current_packet.header);
+                std::cout << "bytes read from header = " << n << '\n';
+
+                if (n < 0)
+                        return;
+
+                std::cout << "len = " << (int)current_packet.header.length << " type = " << (int)current_packet.header.type << '\n';
+                if (current_packet.header.length <= 0)
+                        return;
+
+		if (current_packet.header.type == 1) {
+			current_packet.body = Quote{};
+		} else {
+			current_packet.body = Trade{};
+		}
+
+		std::visit([this](auto&& body) {
+			parse(body, p_args.symbol, current_packet.header.length);
+			if (!first_timestamp) first_timestamp = body.timestamp;
+
+			int diff = (body.timestamp - first_timestamp.value()) / 1'000'000'000ULL;
+			std::cout << "body.timestamp: " << body.timestamp << '\n';
+			std::cout << "Timestamp diff: " << diff << '\n';
+               		std::cout << "is " << p_args.vwap_window_period << " second window reached? " << std::boolalpha << (diff >= p_args.vwap_window_period) << '\n';
+			if (current_quote && seen_trade && diff >= p_args.vwap_window_period) {
+				std::cout << "PROCESSING ORDER\n";
+				process_order(body.timestamp);
+				std::cout << "New timestamp: " << (body.timestamp) << '\n';
+			}
+		}, current_packet.body);
+		std::cout << "---------------------------------\n";
+	}
+
+	void process_order(std::uint64_t timestamp) {
+                ::process_order(p_args.symbol, p_args.side, timestamp, pq_sum / q_sum, current_quote)
+                       	([&] (Order order, std::uint32_t price, std::uint32_t quantity) {
+                               	order.price = price;
+                                order.quantity = std::min(quantity, p_args.max_order_size);
+                                send(p_args.order_connection_socket.handle(), &order, sizeof(order), 0);
+                        });
+
+                pq_sum = q_sum = 0;
+                first_timestamp = timestamp;
+	}
+private:
+	Config& p_args;
+	detail::optional<Quote> current_quote;
+	Packet current_packet;
+	std::optional<std::uint64_t> first_timestamp;
+	int market_sockfd;
+	int pq_sum = 0;
+        int q_sum = 0;
+	bool seen_trade = false;
+private:
+};
+
+int main(int argc, char* argv[]) {
+        using namespace std::chrono_literals;
+
+        if (argc < 9) {
+                std::cout << "Invalid number of arguments\n";
+                std::exit(1);
+        }
+
+        auto config = Config{
+                .symbol = argv[1],
+                .side = argv[2][0],
+                .max_order_size = static_cast<std::uint32_t>(std::stoul(argv[3])),
+                .vwap_window_period = static_cast<std::uint32_t>(std::stoul(argv[4])),
+                .market_data_socket{argv[5], argv[6]},
+                .order_connection_socket{argv[7], argv[8]}
+        };
+
+	Feed<Schema::Packet> feed{config};
+
+
+        while (true) {
+		feed.forward();
+
+		std::this_thread::sleep_for(200ms);
+        }
+}
